b9cdd27ba15c5014d4a33c689654ce649ca99f1f
sanjeev <bhusalsanjeev23@gmail.com>
2023-12-16 23:34:23 +0545

    make navbar responsive

--- app/(platform)/organization/[organizationId]/_components/navbar.tsx
+++ app/(platform)/organization/[organizationId]/_components/navbar.tsx
@@ -8,9 +8,33 @@ function NavBar() {
     <div className="p-4 bg-white border-b">
       <div className="mx-auto max-w-7xl flex justify-between items-center">
         {/* TODO: Implement navbar for small screen */}
-        <Logo />
+        <div className="flex gap-8 md:hidden">
+          <Logo hideText={true} />
+          <OrganizationSwitcher
+            hidePersonal
+            afterSelectOrganizationUrl="/organization/:id"
+            afterCreateOrganizationUrl="/organization/:id"
+            afterLeaveOrganizationUrl="/select-organization"
+            appearance={{
+              elements: {
+                rootBox: {
+                  height: "30px",
+                },
+                organizationPreviewAvatarContainer: {
+                  height: "30px",
+                },
+                organizationPreviewAvatarBox: {
+                  height: "30px",
+                },
+              },
+            }}
+          />
+        </div>
+        <div className="hidden md:block">
+          <Logo />
+        </div>
         <div className="flex gap-12 items-center">
-          <div className="">
+          <div className="hidden md:block">
             <OrganizationSwitcher
               hidePersonal
               afterSelectOrganizationUrl="/organization/:id"
--- app/(platform)/organization/[organizationId]/page.tsx
+++ app/(platform)/organization/[organizationId]/page.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button";
+
 interface OrganizationPageProps {
   params: {
     organizationId: string;
@@ -7,5 +9,11 @@ interface OrganizationPageProps {
 export default function OrganizationPage({
   params: { organizationId },
 }: OrganizationPageProps) {
-  return <div>Organization Id: {organizationId}</div>;
+  return (
+    <div className="mx-auto max-w-7xl px-4 md:p-0">
+      <Button size={"sm"} variant="primary" className="ml-auto block">
+        Create Task
+      </Button>
+    </div>
+  );
 }
