b142a3d44106d25f0dbe7991948b42b834e3bfaa
sanjeev <bhusalsanjeev23@gmail.com>
2023-12-30 14:56:37 +0545

    extract board list to seperate component and css changes

---
+++ app/(platform)/boards/_components/list-board.tsx
@@ -0,0 +1,36 @@
+import { Board } from "@prisma/client";
+import Image from "next/image";
+import Link from "next/link";
+
+interface ListBoardProps {
+  boards: Board[];
+}
+
+function ListBoard({ boards }: ListBoardProps) {
+  return boards.length === 0 ? (
+    <p className="mt-2 text-sm">
+      You donot have any boards yet. Please create a new board
+    </p>
+  ) : (
+    <div className="flex gap-8 flex-wrap">
+      {boards.map((board) => (
+        <Link
+          href={`/boards/${board.id}`}
+          key={board.id}
+          className="w-[100%] md:w-[calc(50%-16px)] lg:w-[calc(33.33%-21.33px)] h-32 relative flex items-center justify-center font-bold rounded-lg cursor-pointer"
+        >
+          <p className="absolute z-40 text-white">{board.name}</p>
+
+          <Image
+            src={board.smallImageUrl}
+            alt="nature picture"
+            fill
+            className="rounded-lg"
+          />
+        </Link>
+      ))}
+    </div>
+  );
+}
+
+export { ListBoard };
--- app/(platform)/boards/page.tsx
+++ app/(platform)/boards/page.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 
 import { NavBar } from "./_components/navbar";
 import { Sidebar } from "./_components/sidebar";
+import { ListBoard } from "./_components/list-board";
 
 export default async function BoardPage() {
   const boards = await getBoards();
@@ -20,29 +21,8 @@ export default async function BoardPage() {
             <h1 className="text-lg font-semibold">Your Boards</h1>
             <CreateBoard />
           </div>
-          <div className="mt-4 flex gap-8 flex-wrap">
-            {boards.length === 0 ? (
-              <p className="mt-2 text-sm">
-                You donot have any boards yet. Please create a new board
-              </p>
-            ) : (
-              boards.map((board) => (
-                <Link
-                  href={`/boards/${board.id}`}
-                  key={board.id}
-                  className="w-[242.5px] lg:w-[232px] xl:w-[320px] h-32 flex items-center justify-center font-bold rounded-lg cursor-pointer grow relative"
-                >
-                  <p className="absolute z-40 text-white">{board.name}</p>
-
-                  <Image
-                    src={board.smallImageUrl}
-                    alt="nature picture"
-                    fill
-                    className="rounded-lg"
-                  />
-                </Link>
-              ))
-            )}
+          <div className="mt-4">
+            <ListBoard boards={boards} />
           </div>
         </div>
       </div>
